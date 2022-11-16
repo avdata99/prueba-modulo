0847434eebbca0c0cf762829a708cebd35f1c9ba
avdata99 <andres@data99.com.ar>
2022-11-16 20:57:41 -0300

    Mi primer paquete

---
+++ paquete/mod1.py
@@ -0,0 +1,5 @@
+def resta(a, b):
+    return a - b
+
+class Carta:
+    pass
--- programa.py
+++ programa.py
@@ -1,4 +1,10 @@
 from modulo import suma
+from paquete.mod1 import Carta, resta
 
 print(suma(8, 9))
+
+c = Carta()
+print(type(c))
+
+print(resta(10, 3))
 
